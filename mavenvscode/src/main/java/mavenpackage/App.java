0254594f5c71d12b2c5db10bb22b6d47a2062a9f
nathan bullock <nbullock1@luc.edu>
2020-07-21 23:14:21 -0500

    Excel File Streams
    All Excel Files imported into memory.

--- mavenvscode/src/main/java/mavenpackage/App.java
+++ mavenvscode/src/main/java/mavenpackage/App.java
@@ -2,12 +2,13 @@ package mavenpackage;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.util.Iterator;
+
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import java.util.ArrayList;
+
+import java.util.*;
 
 /**
  * Hello world!
@@ -17,22 +18,24 @@ public final class App {
     }
     public static void main(String[] args) {
         ArrayList<StudentInfo> studentList = new ArrayList<>();
-
+        Map<Integer, Integer> testScores = new HashMap<>();
+        Map<Integer, Integer> retakeScores = new HashMap<>();
         try{
             File studentInfo = new File("mavenvscode/Student Info.xlsx");
-            FileInputStream stream = new FileInputStream(studentInfo);
+            FileInputStream studentInfoStream = new FileInputStream(studentInfo);
 
-            XSSFWorkbook wb = new XSSFWorkbook(stream);
-            XSSFSheet sheet = wb.getSheetAt(0);
-            Iterator<Row> itr = sheet.iterator();
-            while (itr.hasNext()){
-                StudentInfo student = new StudentInfo();
-                Row row = itr.next();
+            XSSFWorkbook wb1 = new XSSFWorkbook(studentInfoStream);
+            XSSFSheet sheet1 = wb1.getSheetAt(0);
+            Iterator<Row> itr1 = sheet1.iterator();
+            
+            while (itr1.hasNext()){
+                StudentInfo student = new StudentInfo(); //intitialize student object
+                Row row = itr1.next();
                 Iterator<Cell> cellIterator = row.cellIterator(); 
                 while (cellIterator.hasNext()){
                     Cell cell = cellIterator.next();
                     switch (cell.getCellType()){
-                        case Cell.CELL_TYPE_STRING:    
+                        case Cell.CELL_TYPE_STRING:
                             if(cell.getStringCellValue().length() > 1){
                                 student.major = cell.getStringCellValue();
                             }
@@ -41,7 +44,7 @@ public final class App {
                                 student.gender = c;
                             }
                             break;
-                        case Cell.CELL_TYPE_NUMERIC:    
+                        case Cell.CELL_TYPE_NUMERIC:
                             student.studentID = (int) cell.getNumericCellValue();
                             break;
                         default:
@@ -49,13 +52,56 @@ public final class App {
                 }
                 studentList.add(student);
             }
+            wb1.close();
+
+            File testScoresXLSX = new File("mavenvscode/Test Scores.xlsx");
+            FileInputStream testScoresStream = new FileInputStream(testScoresXLSX);
+
+            XSSFWorkbook wb2 = new XSSFWorkbook(testScoresStream);
+            XSSFSheet sheet2 = wb2.getSheetAt(0);
+            Iterator<Row> itr2 = sheet2.iterator();
+            Row tmp = itr2.next();
+            while (itr2.hasNext()){
+                Row row = itr2.next();
+                ArrayList<Integer> arr = new ArrayList<>();
+                for(Cell cell : row){
+                    arr.add((int) cell.getNumericCellValue());
+                }
+                testScores.put(arr.get(0), arr.get(1));
+            }
+            wb2.close();
+
+            File retakeScoresXLSX = new File("mavenvscode/Test Retake Scores.xlsx");
+            FileInputStream retakeScoresStream = new FileInputStream(retakeScoresXLSX);
+
+            XSSFWorkbook wb3 = new XSSFWorkbook(retakeScoresStream);
+            XSSFSheet sheet3 = wb3.getSheetAt(0);
+            Iterator<Row> itr3 = sheet3.iterator();
+            Row tmp2 = itr3.next();
+            while (itr3.hasNext()){
+                Row row = itr3.next();
+                ArrayList<Integer> arr = new ArrayList<>();
+                for(Cell cell : row){
+                    arr.add((int) cell.getNumericCellValue());
+                }
+                retakeScores.put(arr.get(0), arr.get(1));
+            }
+            wb3.close();
         }
         catch(Exception e){
                 e.printStackTrace();
         }
+        
         for(int i =0; i < studentList.size(); i++){
             System.out.println(studentList.get(i).studentID + " | " + studentList.get(i).major + " | " + studentList.get(i).gender);
         }
-        
+        for (Map.Entry<Integer, Integer> entry : testScores.entrySet()){
+            System.out.println("Key = " + entry.getKey() + 
+                             ", Value = " + entry.getValue());
+        }
+        for (Map.Entry<Integer, Integer> entry : testScores.entrySet()){
+            System.out.println("Key = " + entry.getKey() + 
+                             ", Value = " + entry.getValue());
+        }
     }
 }
--- mavenvscode/src/main/java/mavenpackage/StudentInfo.java
+++ mavenvscode/src/main/java/mavenpackage/StudentInfo.java
@@ -4,11 +4,15 @@ public final class StudentInfo{
     public int studentID;
     public String major;
     public char gender;
+    public int testScore;
+    public int retakeScore;
 
-    public StudentInfo(int studentID, String major, char gender){
+    public StudentInfo(int studentID, String major, char gender, int testScore, int retakeScore){
         this.studentID = studentID;
         this.major = major;
         this.gender = gender;
+        this.testScore = testScore;
+        this.retakeScore = retakeScore;
     }
     public StudentInfo() {}
 
