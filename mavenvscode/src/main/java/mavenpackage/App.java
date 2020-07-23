b2699d02ebbe3577815cb5e0a5149be0ecae900e
nathan bullock <nbullock1@luc.edu>
2020-07-23 10:58:31 -0500

    JSON Request

--- mavenvscode/src/main/java/mavenpackage/App.java
+++ mavenvscode/src/main/java/mavenpackage/App.java
@@ -1,8 +1,16 @@
+/**
+ * Program by Nathan Bullock
+ * Email: nbullock1@luc.edu
+ * Phone: 317-224-9823
+ */
 package mavenpackage;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.*;
 
 import org.apache.poi.ss.usermodel.Cell;
@@ -11,7 +19,11 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.json.*;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Collections;
 
 /**
  * Hello world!
@@ -21,7 +33,8 @@ public final class App {
     }
 
     public static void main(String[] args) throws IOException {
-        ArrayList<StudentInfo> studentList = new ArrayList<>();
+        ArrayList<Student> studentList = new ArrayList<>();
+        ArrayList<Integer> femaleIDs = new ArrayList<>();
         Map<Integer, Integer> testScores = new HashMap<>();
         Map<Integer, Integer> retakeScores = new HashMap<>();
         try{
@@ -31,9 +44,9 @@ public final class App {
             XSSFWorkbook wb1 = new XSSFWorkbook(studentInfoStream);
             XSSFSheet sheet1 = wb1.getSheetAt(0);
             Iterator<Row> itr1 = sheet1.iterator();
-            Row skip = itr1.next(); //skip first row
+            itr1.next(); //skip first row
             while (itr1.hasNext()){
-                StudentInfo student = new StudentInfo(); //intitialize student object
+                Student student = new Student(); //intitialize student object
                 Row row = itr1.next();
                 Iterator<Cell> cellIterator = row.cellIterator(); 
                 while (cellIterator.hasNext()){
@@ -57,14 +70,13 @@ public final class App {
                 studentList.add(student);
             }
             wb1.close();
-
+            //populate map with test scores from xlsx
             File testScoresXLSX = new File("mavenvscode/Test Scores.xlsx");
             FileInputStream testScoresStream = new FileInputStream(testScoresXLSX);
-
             XSSFWorkbook wb2 = new XSSFWorkbook(testScoresStream);
             XSSFSheet sheet2 = wb2.getSheetAt(0);
             Iterator<Row> itr2 = sheet2.iterator();
-            Row tmp = itr2.next(); //skip first row
+            itr2.next(); //skip first row
             while (itr2.hasNext()){
                 Row row = itr2.next();
                 ArrayList<Integer> arr = new ArrayList<>();
@@ -81,7 +93,7 @@ public final class App {
             XSSFWorkbook wb3 = new XSSFWorkbook(retakeScoresStream);
             XSSFSheet sheet3 = wb3.getSheetAt(0);
             Iterator<Row> itr3 = sheet3.iterator();
-            Row tmp2 = itr3.next(); // skip first row
+            itr3.next(); // skip first row
             while (itr3.hasNext()){
                 Row row = itr3.next();
                 ArrayList<Integer> arr = new ArrayList<>();
@@ -95,21 +107,7 @@ public final class App {
         catch(Exception e){
                 e.printStackTrace();
         }
-        
-        for(int i =0; i < studentList.size(); i++){
-            System.out.println(studentList.get(i).studentID + " | " + studentList.get(i).major + " | " + studentList.get(i).gender);
-        }
-        System.out.println("Test Scores: ");
-        for (Map.Entry<Integer, Integer> entry : testScores.entrySet()){
-            System.out.println("Key = " + entry.getKey() + 
-                             ", Value = " + entry.getValue());
-        }
-        System.out.println("Retake Scores: ");
-        for (Map.Entry<Integer, Integer> entry : testScores.entrySet()){
-            System.out.println("Key = " + entry.getKey() + 
-                             ", Value = " + entry.getValue());
-        }
-        //using student ids, set test score for each student object
+        //using student ids, set test score and retake score for each student object
         for(int i =0; i < studentList.size(); i++){
             int id = studentList.get(i).studentID;
             for(Map.Entry<Integer, Integer> entry : testScores.entrySet()){
@@ -123,9 +121,10 @@ public final class App {
                 }
             }
         }
-        ArrayList<Integer> femaleIDs = new ArrayList<>();
-        double sum =0;
-        String major = studentList.get(2).major;
+        System.out.println(" ");
+        double sum =0; //sum of final test scores
+        String major = studentList.get(2).major; //To get comparable string value of computer science major 
+        //print out all student objects and final test scores while also adding female computer science majors to array
         for(int i =0; i < studentList.size(); i++){
             System.out.println("Student ID: " + studentList.get(i).studentID);
             System.out.println("Major: " + studentList.get(i).major);
@@ -135,29 +134,59 @@ public final class App {
             System.out.println("Final Test Score: " + studentList.get(i).getFinalTestScore());
             sum += studentList.get(i).getFinalTestScore();
             if((studentList.get(i).gender == 'F') && (studentList.get(i).major == major)){
-                System.out.println(studentList.get(i).studentID);
                 femaleIDs.add(studentList.get(i).studentID);
             }
             System.out.println(" ");
         }
+
         Collections.sort(femaleIDs);
-        System.out.println("");
-        System.out.println("Sorted Female IDs: ");
         int femaleArr[] = new int [femaleIDs.size()];
         for(int i =0; i < femaleIDs.size(); i++){
             femaleArr[i] = femaleIDs.get(i);
         }
+        System.out.println("");
+        System.out.println("Sorted Female IDs: ");
+        for(int i =0; i < femaleArr.length; i++){
+            System.out.println(femaleArr[i]);
+        }
+        System.out.println("");
         int classAverage = (int) sum / studentList.size();
+
+        System.out.println("Class Average: " + classAverage);
+        System.out.println(" ");
+
+        //create JSONObject with data
         JSONObject obj = new JSONObject();
         obj.put("id", "nbullock1@luc.edu");
         obj.put("name", "Nathan Bullock");
         obj.put("average", classAverage);
         obj.put("studentIds", femaleArr);
+
+
         URL url = new URL ("http://54.90.99.192:5000/challenge");
-        URLConnection con = url.openConnection();
-        HttpURLConnection http = (HttpURLConnection) con;
-        http.setRequestMethod("POST");
-        http.setDoOutput(true);
-        System.out.println("Class Average: " + classAverage);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("POST");
+        con.setDoOutput(true);
+        con.setRequestProperty("Content-Type", "application/json; utf-8");
+        con.setRequestProperty("Accept", "application/json");
+
+        String jsonString = obj.toString();
+        System.out.println("JSON Request String: ");
+        System.out.println(jsonString);
+        con.connect();
+        try(OutputStream os = con.getOutputStream()){
+            byte[] input = jsonString.getBytes("utf-8");
+            os.write(input, 0, input.length);
+        }
+        System.out.println("");
+        System.out.println("Server Response: ");
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(), "utf-8"))) {
+            StringBuilder response = new StringBuilder();
+            String responseLine = null;
+            while ((responseLine = br.readLine()) != null) {
+                response.append(responseLine.trim());
+            }
+            System.out.println(response.toString());
+        }
     }
 }
--- mavenvscode/src/main/java/mavenpackage/Student.java
+++ mavenvscode/src/main/java/mavenpackage/Student.java
@@ -1,20 +1,20 @@
 package mavenpackage;
 
-public final class StudentInfo{
+public final class Student{
     public int studentID;
     public String major;
     public char gender;
     public int testScore;
     public int retakeScore;
 
-    public StudentInfo(int studentID, String major, char gender, int testScore, int retakeScore){
+    public Student(int studentID, String major, char gender, int testScore, int retakeScore){
         this.studentID = studentID;
         this.major = major;
         this.gender = gender;
         this.testScore = testScore;
         this.retakeScore = retakeScore;
     }
-    public StudentInfo() {}
+    public Student() {}
 
     void setStudentID(int studentID){
         this.studentID = studentID;
